305b5ac8d9f6964cd35ba323e0ea48e57a6b7ee6
sandeepkumaaarr <kumarsandeep1031@gmail.com>
2022-05-06 15:00:09 +0530

    arithmatic changes

---
+++ src/components/Arithmaticopp.js
@@ -0,0 +1,14 @@
+function Sum(a,b) {
+  return (a+b);
+}
+function Mul(a,b) {
+    return (a*b);
+  }
+function Add(a,b) {
+    return (a+b);
+  }
+function Sub(a,b) {
+    return (a-b);
+  }
+
+export  {Sum,Mul,Add,Sub};
---
+++ src/components/Body.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Sum,Mul,Add,Sub} from './Arithmaticopp'
+let vara = +prompt("A");
+let varb = +prompt("B");
+function Body() {
+ 
+
+  return (
+    <>
+    
+    <h1>Sum is {Sum(vara,varb)}</h1>
+    <h1>Mul is {Mul(vara,varb)}</h1>
+    <h1>Add is {Add(vara,varb)}</h1>
+    <h1>Sub is {Sub(vara,varb)}</h1>
+    </>
+  )
+}
+
+export default Body
